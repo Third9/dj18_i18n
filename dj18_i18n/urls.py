64b9b6dd80afed1926963577c3443f526ec296cb
Hyeock Kwon <kwonh85@kinx.net>
2017-02-14 16:45:06 +0900

    -. JavaScript 코드도 다국어 지원

--- dj18_i18n/urls.py
+++ dj18_i18n/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.i18n import javascript_catalog
 from django.conf.urls.i18n import i18n_patterns
 from django.http import HttpResponse
 from myapp.views import MyApp
 
+js_info_dict = {
+    'packages': ('dj18_i18n')
+}
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
@@ -12,4 +16,5 @@ urlpatterns = [
 
 urlpatterns += i18n_patterns(
     url(r'^$', MyApp.as_view(), name='home'),
+    url(r'^jsi18n/$', javascript_catalog, js_info_dict, name='javascript-catalog'),
 )
---
+++ static/js/myapps.js
@@ -0,0 +1,5 @@
+var jsI18N = document.getElementById('js_i18n');
+var jsTitle = gettext("__js_title__");
+var jsBody = gettext("__js_body__");
+
+jsI18N.innerHTML = '<h5>'+jsTitle+'</h5><h5>'+jsBody+'</h5>';
