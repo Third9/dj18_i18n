694dd7639a99c56099e0d34a01aadc10f1329628
Hyeock Kwon <kwonh85@kinx.net>
2017-02-14 16:39:08 +0900

    -. 프로젝트 초기화 -. i18n 기본 작업

---
+++ myapp/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.utils.translation import ugettext as _
+
+from django.utils import translation
+from django.utils.translation import activate
+
+
+# Create your views here.
+class MyApp(TemplateView):
+    def get(self, request, *args, **kwargs):
+        current_path = request.path
+
+        return render(request,
+                      context={'serverTitle_i18n': _("__server_title__"),
+                               'serverBody_i18n': _("__server_body__")},
+                      template_name='myapps.html')
+
+    def post(self, request, *args, **kwargs):
+        pass
